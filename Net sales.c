8fafcd880b9b949a488ceb2843dd065ea215be1b
harshilg23 <gadhaviharshildan3@gmail.com>
2025-08-28 15:44:31 +0530

    Add files via upload

---
+++ Net sales.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    float gross;
+    printf("Enter gross amount: ");
+    scanf("%f", &gross);
+    float net = gross - 0.10f * gross;
+    printf("Net amount = %.2f\n", net);
+}
