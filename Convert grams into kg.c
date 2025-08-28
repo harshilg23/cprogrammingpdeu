884fdc41c3933419560ce048e67e6fd649383081
harshilg23 <gadhaviharshildan3@gmail.com>
2025-08-28 15:37:34 +0530

    Add files via upload

---
+++ Convert grams into kg.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    float a,b;
+    printf("Insert weight in grams");
+    scanf("%f",&a);
+    b=a/1000;
+    printf("%f g=%fkg\n",a,b);
+
+}
---
+++ Convert kg into grams.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+    float a,b;
+    printf("Insert weight in kilogram:");
+    scanf("%f",&a);
+    b=a*1000;
+    printf("%f kg=%f g\n",a,b);
+}
