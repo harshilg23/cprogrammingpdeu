7b293ed7fe7a6948c64b164b0743b0f5ed5ffccf
harshilg23 <gadhaviharshildan3@gmail.com>
2025-08-28 15:46:42 +0530

    Add files via upload

---
+++ Average & total of three subject.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    float s1, s2, s3;
+    printf("Enter three marks: ");
+    scanf("%f %f %f", &s1, &s2, &s3);
+    float total = s1 + s2 + s3;
+    float avg = total / 3.0f;
+    printf("Total = %.2f\nAverage = %.2f\n", total, avg);
+}
