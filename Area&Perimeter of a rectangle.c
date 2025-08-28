700c483a7c54a5c6a71a78e7c2bb01e8685a117c
harshilg23 <gadhaviharshildan3@gmail.com>
2025-08-28 15:42:38 +0530

    Add files via upload

---
+++ Area&Perimeter of a rectangle.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+    float L, B;
+    printf("Enter length and breadth: ");
+    scanf("%f %f", &L, &B);
+    printf("Area = %.2f\nPerimeter = %.2f\n", L*B, 2*(L+B));
+}
