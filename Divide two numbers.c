c8a23e207bdedf03aacc72c5f988fa57582a274f
harshilg23 <gadhaviharshildan3@gmail.com>
2025-08-28 15:31:26 +0530

    Add files via upload

---
+++ Divide two numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+float main()
+{
+    float a,b,c;
+    printf("Enter a number");
+    scanf("%f",&a);
+    printf("Enter a number");
+    scanf("%f",&b);
+    c=a/b;
+    printf("%f/%f=%f\n",a,b,c);
+
+}
+
+
+
---
+++ Multiply two numbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b,c;
+    printf("Enter a numbetr");
+    scanf("%d",&a);
+    printf("Enter a number");
+    scanf("%d",&b);
+    c=a*b;
+    printf("%d*%d=%d\n",a,b,c);
+
+}
